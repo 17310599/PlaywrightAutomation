eaac09e03136f69e208718697416ca032c58cba5
Pallikala Rachitha <pallikala.10834552@ltimindtree.com>
2025-08-19 12:00:26 +0530

    committing new file to Branch1

---
+++ tests/POM_Tests/newFile1.spec.js
@@ -0,0 +1,12 @@
+import { LoginCases} from "../POM_Pages/LoginCases";
+import {test, expect} from "@playwright/test";
+
+test.describe.configure({ mode: 'serial' });
+test("Signing in with valid username and password", async({page})=>{
+
+    const loginObj = new LoginCases(page)
+    await loginObj.login("rachitha", "password")
+    await page.waitForTimeout(5000)
+    await expect(await page.locator(loginObj.logoutBtn)).toHaveText("Log out")
+
+})
